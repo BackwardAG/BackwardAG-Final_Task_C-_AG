6a03a57c07ee546822c9096be4e7e403869fdc78
Armen G <ar.gukasov@gmail.com>
2023-02-28 21:03:02 +0300

    added file

---
+++ Final_Task_Different/Program.cs
@@ -0,0 +1,39 @@
+﻿// Написать программу, которая из имеющегося массива 
+// строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте 
+// выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+// Примеры:
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+string[] Extract(string[] array)
+{
+    string[] array2 = new string[array.Length];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[j] = array[i];
+            j++;
+        }
+    }
+    string[] arrayNew = new string[j];
+    Array.Copy(array2, arrayNew, j);
+    return arrayNew;
+
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] array = {"hello", "2", "world", ":-)", "1234", "1567",
+"-2", "computer science", "Russia", "Denmark", "Kazan"};
+PrintArray(Extract(array));
